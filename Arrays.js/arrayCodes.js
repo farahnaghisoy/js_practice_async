abea6bce700b95c5872e387e2833e0ca9c81d48a
farahnaghisoy <ferehagisoy7@gmail.com>
2025-02-08 01:25:57 +0400

    add line

--- Arrays.js/arrayCodes.js
+++ Arrays.js/arrayCodes.js
@@ -59,15 +59,55 @@ const flowers =
 //     updateLesson(lesson, "LiveScript ", "Advanced");
 // console.log(lesson);
 
-let company = {
- name: "Adviad",
- employess: [
-    {name: "Vugar", position: "Software", salary: 2050 },
-    {name: "Farah", position: "Developer", salary: 1000 },
- ]
-};
-    console.log(company.employess[0].name);
-    console.log(company.employess[1].position);
+// let company = {
+//  name: "Adviad",
+//  employess: [
+//     {name: "Vugar", position: "Software", salary: 2050 },
+//     {name: "Farah", position: "Developer", salary: 1000 },
+//  ]
+// };
+//     console.log(company.employess[0].name);
+//     console.log(company.employess[1].position);
+
+// --------------------------------------
+// let person = {
+//     name: "Farah",
+//     greet: function(){
+//         return `Hi, I'm ${this.name}!`;
+//     }
+// };
+// person.hobby = "yoga making"
+// // console.log(person.heyo());
+// // console.log(Object.keys(person));
+// console.log(Object.values(person));
+// // console.log(Object.entries(person));
+//  ----------------------------------------------
+// Class
+// class Instrument{
+//     constructor (model1, model2, year1, year2 ){
+//         this.model1 = model1;
+//         this.model2 = model2;
+//         this.year1 = year1;
+//         this.year2 = year2;
+//     }
+//     start(){
+//         console.log(`${this.model1}   ${this.year1} prepared.`);
+//     }
+// }
+// let musicalInstrum = new Instrument("violin", 2021, 2025 );
+// musicalInstrum.start();
+// ----------------------------------------------------------
+// JSON 
+// let user = {name:"Bararos", age: 56};
+//     // obj stringify formatina chevrildi
+// let jsonStr = JSON.stringify(user);
+// console.log(jsonStr);
+
+// // JSON stringini obyektə çeviririk
+// let userObj = JSON.parse(jsonStr);
+// console.log(userObj);
+
+
     
 
 
